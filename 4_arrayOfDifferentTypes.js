35c37f94ce6a585d336f5bd8fdcf1f33e38b5bcb
MayankGupta <mayank.gupta.6.88@gmail.com>
2020-04-06 11:44:04 +0530

    Changes

---
+++ 4_arrayOfDifferentTypes.js
@@ -0,0 +1,15 @@
+var arrayData = [ "1", 2, false, null, "5", {name: "Mayank", age: 10}, [10, 20, 30] ]
+
+var iterateData = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var sum = 0;
+
+for(var i = 0; i <= iterateData.length -1; i++) {
+
+    //Operation to be performed
+
+
+    sum = sum + iterateData[i];
+}
+
+console.log(sum)
