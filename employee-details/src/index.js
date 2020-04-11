4e0af4c59d35a8a8852e2381e00a080f6265f6c0
MayankGupta <mayank.gupta.6.88@gmail.com>
2020-04-11 18:07:42 +0530

    Updates

---
+++ employee-details/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var userName = "Anshul";
+
+ReactDOM.render(<div><h1>Hello { 1 + 1 }</h1><p>This is sample paragraph</p><b>This is React Startup Application</b></div>, document.getElementById("root"));
