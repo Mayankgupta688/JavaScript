a46b94005ecb15a42e087bac328361c6f84e091a
MayankGupta <mayank.gupta.6.88@gmail.com>
2020-04-24 17:55:25 +0530

    Changes

--- Node/server.js
+++ Node/server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var fs = require("fs");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/data", function(req, res) {
     fs.readFile("./data.json", function(err, data) {
@@ -11,7 +14,7 @@ app.get("/data", function(req, res) {
 
 app.get("/data/:id", function(req, res) {
     fs.readFile("./data.json", function(err, data) {
-        let studentList = JSON.parse(data)
+        let studentList = JSON.parse(data);
         studentList.employeeData = studentList.employeeData.filter(function(emp) {
             if(emp.id == req.params.id) {
                 return true;
@@ -19,12 +22,31 @@ app.get("/data/:id", function(req, res) {
                 return false;
             }
         })
-        res.send(studentList.employeeData[0])
+        res.send(studentList.employeeData[0]);
     })
 })
 
+app.post("/postdata", function(req, res) {
+    var userName = req.body.userName;
+    res.send("Application processed for the user: " + userName);
+})
+
+app.get("/postdata", function(req, res) {
+    var userName = req.query.userName;
+    res.send("Application processed for the user: " + userName);
+})
+
 app.get("/", function(req, res) {
-    res.send("Hello World...")
+    fs.readFile("./templates/index.html", function(err, data) {
+        res.send(data.toString());
+    })
+    var a = 10;
+    console.log(a)
+})
+
+app.get("/async", function(req, res) {
+    var data = fs.readFileSync("./templates/index.html");
+    res.send(data.toString());
 })
 
 app.listen(8000);
